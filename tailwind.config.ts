55f27984cec969ba9b80dbd3cb05691142cea054
Marcos Aurélio <marcosaureliolelo@outlook.com>
2023-09-08 20:12:12 -0300

    feat: adding color theme to Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'negative': '#EA3943',
+        'positive': '#00B772',
       },
     },
   },
