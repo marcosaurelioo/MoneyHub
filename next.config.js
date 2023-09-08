417831af0a0ff8598981318fc72320843c1669b3
Marcos Aurélio <marcosaureliolelo@outlook.com>
2023-09-08 20:08:04 -0300

    feat: adding next.js images config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        pathname: "/static/img/coins/*/*",
+        hostname: "s2.coinmarketcap.com",
+        protocol: "https",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
